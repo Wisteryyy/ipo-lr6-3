84d9a44b754e0130346ed869d34d7828fd036110
Sofia Skakun <sofiaskakun30@gmail.com>
2024-11-24 11:42:56 +0300

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-kolichestvo_strok=int(input("Введите количество строк: ")) #запрос на ввод
-if kolichestvo_strok <=0: # если условие правдиво
-    print("Количество строк не может быть отрицательным и равным нулю.") # выводим это
-    
-list = [] # список создаем пустой
-for i in range(kolichestvo_strok): #цикл который выполняется kolichestvo_strok раз
-    stroki = input(f"Введите строку {i + 1}: ") # вводим строку
-    list.append(stroki) #добавляем ее в список
+number_of_lines = int(input("Введите количество строк: "))  # запрос на ввод количества строк
+if number_of_lines <= 0:  # проверка на количество строк
+    print("Количество строк не может быть отрицательным и равным нулю.")  # вывод
 
-unucalnie_stroki = set() # создаем пустое множество
+lines_list = []  # пустой список для строк
+for i in range(number_of_lines):  # цикл, выполняющийся number_of_lines раз
+    input_line = input(f"Введите строку {i + 1}: ")  # ввод строки
+    lines_list.append(input_line)  # добавляем строку в список
 
-for stroki in list: #перебераем строки в списке
-    w = stroki.split() #делим по пробелам и присваеиваем w
-    ww = [word.lower() for word in w]  # приводим к нижнему регистру
-    unucalnie_stroki.update(ww) # обновляем наше множество
+unique_words = set()  # множество
 
-print(f"Количество различных слов: {len(unucalnie_stroki)}") # и выводим количество(len) различных(set) слов
+for line in lines_list:  # перебираем строки в списке
+    words = line.split()  # разделяем строку по пробелам
+    words_lower = [word.lower() for word in words]  # приводим каждое слово к нижнему регистру
+    unique_words.update(words_lower)  # обновляем множество
+
+print(f"Количество различных слов: {len(unique_words)}")  # выводим
